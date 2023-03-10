6d87c9f001592f7b7bac53460942ee573632671c
Ophelia <ophelia.lavey2@gmail.com>
2023-03-10 20:08:39 +0100

    code ready to submit now

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -23,17 +23,17 @@ namespace CBApp.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // For running unit tests ONLY uncomment:
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder
-                .UseLazyLoadingProxies().
-                UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-CBApp-0EFDC2CE-E197-4185-AEEB-8A5182EBB5B5;Trusted_Connection=True;MultipleActiveResultSets=true");
-            }
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder
+            //    .UseLazyLoadingProxies().
+            //    UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-CBApp-0EFDC2CE-E197-4185-AEEB-8A5182EBB5B5;Trusted_Connection=True;MultipleActiveResultSets=true");
+            //}
 
             // Use this when not running unit tests
-            //optionsBuilder
-            //.UseLazyLoadingProxies().
-            //UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-CBApp-0EFDC2CE-E197-4185-AEEB-8A5182EBB5B5;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder
+            .UseLazyLoadingProxies().
+            UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-CBApp-0EFDC2CE-E197-4185-AEEB-8A5182EBB5B5;Trusted_Connection=True;MultipleActiveResultSets=true");
 
         }
 
